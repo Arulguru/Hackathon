94bf805ae93eedc8726187c78aff9ad736123800
Arulguru <18euec021@skcet.ac.in>
2020-09-11 11:08:08 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector(".btn-primary")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" +name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
+
+
